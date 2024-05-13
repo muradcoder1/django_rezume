84eec4e262d1d1e9ddd814b0fa80e51dc5ad4491
muradcoder1 <huseyinovai40@gmail.com>
2024-05-13 19:40:24 +0400

    homeworkis done

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+def about_us(request):
+    return render(request, "about-us.html")
+def services(request):
+    return render(request, "services.html")
+def portfolio(request):
+    return render(request, "portfolio.html")
+def elements(request):
+    return render(request, "elements.html")
+def blog(request):
+    return render(request, "blog.html")
+def single_blog(request):
+    return render(request, "single-blog.html")
+def contact(request):
+    return render(request, "contact.html")
